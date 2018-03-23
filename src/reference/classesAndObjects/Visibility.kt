79af7626048246bab07f58d4eb5e8c05765d13a7
Yoonkwan Kim <yoonkwan.kim@infor.com>
2018-03-23 17:36:55 -0400

    .

---
+++ src/reference/classesAndObjects/Visibility.kt
@@ -0,0 +1,14 @@
+package reference.classesAndObjects
+
+/** Top-level properties directly inside a package. Default public is used */
+fun defaultPulbicFunction() {}
+class DefaultPublicClass {}
+
+
+/** visible inside the same module */
+internal val visibleInsideTheSameModule = 0
+
+/** visible inside a file */
+private val visibleInsideAFile = 0
+
+/** */

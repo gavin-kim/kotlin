94ded6110541de24efa900b00197ebffe6b930e5
Yoonkwan Kim <yoonkwan.kim@infor.com>
2018-03-23 17:19:00 -0400

    .

---
+++ src/reference/classesAndObjects/Interfaces.kt
@@ -0,0 +1,28 @@
+package reference.classesAndObjects
+
+interface AAA {
+    fun foo() { println("A") }
+    fun bar()
+}
+
+interface BBB {
+    fun foo() { println("B") }
+    fun bar() { println("bar") }
+}
+
+class CCC : AAA {
+    override fun bar() {
+        println("bar")
+    }
+}
+
+class DDD : AAA, BBB {
+    override fun foo() {
+        super<AAA>.foo()
+        super<BBB>.foo()
+    }
+
+    override fun bar() {
+        super.bar()
+    }
+}
